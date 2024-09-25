76361b865a09487900a4433cdf5555be3f3bd52a
CS24786 <nawaztalha078@gmail.com>
2024-09-25 12:54:38 +0500

    task 13

---
+++ task13.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ int x,y;
+ cout<<"Enter a number = ";
+ cin>>x;
+ cout<<"Enter a number = "<< x+y <<endl;
+ cin>>y;
+ cout<<"Enter a number = "<< x+y <<endl;
+ cin>>y;
+ cout<<"Enter a number = "<< x+y <<endl;
+ cin>>y;
+ cout<<"Enter a number = "<< x+y <<endl;
+ cin>>y;
+ cout<<"The sum of 5 integers is = "<< x <<endl;
+ return 0;
+}
+
+
