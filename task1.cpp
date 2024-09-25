90abaf039625c51a4b75bd56fdc92adcdebac3c1
CS24786 <nawaztalha078@gmail.com>
2024-09-25 12:35:39 +0500

    task 1

---
+++ task1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+  int n;
+  cout<<"Enter the number of sides of a polygon= ";
+  cin>>n;
+  cout<<"sum of internal angles= " << (n-2) * 180 <<endl;
+}
