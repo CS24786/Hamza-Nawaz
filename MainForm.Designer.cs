94bdeacc007ca939c3fbe9919621384fbe7c2abb
CS24786 <nawaztalha078@gmail.com>
2025-03-01 00:13:09 +0500

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace FacultyWorkLoadSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlHeader = new Panel();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            panel2 = new Panel();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            richTextBox1 = new RichTextBox();
+            pnlHeader.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            pnlHeader.BackColor = SystemColors.ActiveCaption;
+            pnlHeader.Controls.Add(button2);
+            pnlHeader.Controls.Add(button1);
+            pnlHeader.Controls.Add(label1);
+            pnlHeader.Location = new Point(12, 12);
+            pnlHeader.Name = "pnlHeader";
+            pnlHeader.Size = new Size(776, 118);
+            pnlHeader.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.MenuText;
+            label1.Location = new Point(195, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(342, 60);
+            label1.TabIndex = 1;
+            label1.Text = "    FACULTY WORKLOAD AND \r\nRESOURCE ALLOCATION SYSTEM";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(623, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 37);
+            button1.TabIndex = 2;
+            button1.Text = "LOGOUT";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(0, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 37);
+            button2.TabIndex = 1;
+            button2.Text = "___\r\n___";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(3, 11);
+            label2.Name = "label2";
+            label2.Size = new Size(153, 25);
+            label2.TabIndex = 1;
+            label2.Text = "FACULTY NAME";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(171, 136);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(397, 286);
+            panel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(343, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(3, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(157, 25);
+            label3.TabIndex = 3;
+            label3.Text = "COURSES NAME";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(3, 117);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(343, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(3, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(223, 25);
+            label4.TabIndex = 5;
+            label4.Text = "WORKLOAD SUMMARY";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(3, 180);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(343, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(3, 215);
+            label5.Name = "label5";
+            label5.Size = new Size(229, 25);
+            label5.TabIndex = 7;
+            label5.Text = "RESOURCE ALLOCATION";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.AppWorkspace;
+            panel2.Controls.Add(richTextBox1);
+            panel2.Controls.Add(label6);
+            panel2.Location = new Point(171, 428);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(397, 150);
+            panel2.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(3, 245);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(343, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(3, 11);
+            label6.Name = "label6";
+            label6.Size = new Size(135, 25);
+            label6.TabIndex = 0;
+            label6.Text = "DESCRIPTION";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(3, 39);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(375, 96);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(800, 581);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pnlHeader);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainForm";
+            pnlHeader.ResumeLayout(false);
+            pnlHeader.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlHeader;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private Panel panel1;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private Panel panel2;
+        private RichTextBox richTextBox1;
+        private Label label6;
+    }
+}
